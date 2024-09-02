119aab9b49c49e379e41899f571abb1fb6715275
mahabib19999 <96052746+mahabib19999@users.noreply.github.com>
2024-09-02 16:25:50 +0600

    Create nubit.sh

---
+++ nubit.sh
@@ -0,0 +1,19 @@
+/_____/  /_____/  /_____/  /_____/  /_____/ 
+
+Started nubit DA node 
+node version: 
+node type:      light
+network:        nubit-alphatestnet-1
+
+/_____/  /_____/  /_____/  /_____/  /_____/ 
+
+The p2p host is listening on:
+*  /ip4/127.0.0.1/udp/2121/quic-v1/webtransport/certhash/uEiB9lqoC_TeHP9bz4PWEDSUMlRMaOvVPhglAy63LtB-Qnw/certhash/uEiARTDqDNRTvmD7BKvwElMVEsySwmOY8AN6yvCPTs4Dp3w/p2p/12D3KooWSZmZWjyNbjy4t6mADjWzxWRa9KnfbwtTxtWhVh9Pc64i
+*  /ip4/172.22.173.67/tcp/2121/p2p/12D3KooWSZmZWjyNbjy4t6mADjWzxWRa9KnfbwtTxtWhVh9Pc64i
+*  /ip4/172.22.173.67/udp/2121/quic-v1/p2p/12D3KooWSZmZWjyNbjy4t6mADjWzxWRa9KnfbwtTxtWhVh9Pc64i
+*  /ip4/172.22.173.67/udp/2121/quic-v1/webtransport/certhash/uEiB9lqoC_TeHP9bz4PWEDSUMlRMaOvVPhglAy63LtB-Qnw/certhash/uEiARTDqDNRTvmD7BKvwElMVEsySwmOY8AN6yvCPTs4Dp3w/p2p/12D3KooWSZmZWjyNbjy4t6mADjWzxWRa9KnfbwtTxtWhVh9Pc64i
+*  /ip4/183.173.124.169/udp/58827/quic-v1/p2p/12D3KooWSZmZWjyNbjy4t6mADjWzxWRa9KnfbwtTxtWhVh9Pc64i
+*  /ip4/183.173.124.169/udp/58827/quic-v1/webtransport/certhash/uEiB9lqoC_TeHP9bz4PWEDSUMlRMaOvVPhglAy63LtB-Qnw/certhash/uEiARTDqDNRTvmD7BKvwElMVEsySwmOY8AN6yvCPTs4Dp3w/p2p/12D3KooWSZmZWjyNbjy4t6mADjWzxWRa9KnfbwtTxtWhVh9Pc64i
+*  /ip6/::1/tcp/2121/p2p/12D3KooWSZmZWjyNbjy4t6mADjWzxWRa9KnfbwtTxtWhVh9Pc64i
+*  /ip6/::1/udp/2121/quic-v1/p2p/12D3KooWSZmZWjyNbjy4t6mADjWzxWRa9KnfbwtTxtWhVh9Pc64i
+*  /ip6/::1/udp/2121/quic-v1/webtransport/certhash/uEiB9lqoC_TeHP9bz4PWEDSUMlRMaOvVPhglAy63LtB-Qnw/certhash/uEiARTDqDNRTvmD7BKvwElMVEsySwmOY8AN6yvCPTs4Dp3w/p2p/12D3KooWSZmZWjyNbjy4t6mADjWzxWRa9KnfbwtTxtWhVh9Pc64i
